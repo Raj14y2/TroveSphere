8594723ca3eb329964c6d7f9c0bc272b6c162de0
Raj14y2 <rajashekarkp05@gmail.com>
2024-05-25 11:50:35 +0530

    new commit done

---
+++ src/main/java/Sripts2/TestScript.java
@@ -0,0 +1,12 @@
+package Sripts2;
+
+public class TestScript 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("hi how are you");
+		System.out.println("then whats app");
+		
+	}
+
+}
